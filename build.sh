74cdc4f4faf07f479bc6b35e91b60c21fd4b72bd
Strit <danjohansen@strits.dk>
2018-01-21 11:30:21 +0100

    updated build script

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#Define variables
-WORKSPACE="/home/strit/build-dir"
 #Display messages
  msg() {
     ALL_OFF="\e[1;0m"
@@ -11,61 +9,60 @@ WORKSPACE="/home/strit/build-dir"
       printf "${GREEN}==>${ALL_OFF}${BOLD} ${mesg}${ALL_OFF}\n" "$@" >&2
  }
 
+#Build the 'any' packages on armv7
 if [ $3 == "any" ]; then
 	_ARCH="armv7"
 else
 	_ARCH="$3"
 fi
 
-_DATE=$(date +"%Y-%m-%d_%H%M%S")
-_LOGDIR=$WORKSPACE/logs/$1/$2-$3-$_DATE.log
+######## Some globals to set ########
+#Only set this one
+_BUILDDIR=/home/strit/build-dir
+
+#Leave these alone
+_ROOTFS=$_BUILDDIR/$_ARCH
+_REPODIR=$_BUILDDIR/repo
+_PKGDIR=$_BUILDDIR/packages
+
+
 
 #enable qemu binaries
-msg "Enabling qemu binaries"
-#printf "\n\n===== Enabling qemu binaries =====\n" >> "$_LOGDIR"
+msg "===== Enabling qemu binaries ====="
 sudo update-binfmts --enable qemu-arm
 sudo update-binfmts --enable qemu-aarch64 
 
 #pulling git repos
-msg "Updating the github repo"
-#printf "\n\n===== Updating the github repo =====\n" >> "$_LOGDIR"
-#for dir in $WORKSPACE/repo/*; do (cd "$dir" && git pull); done #2>&1 >> "$_LOGDIR"
+#msg "===== Updating the github repo ====="
+#for dir in $_REPODIR/*; do (cd "$dir" && git pull); done
 
 #updating rootfs 
-msg "Updating the rootfs"
-#printf "\n\n===== Updating the rootfs =====\n" >> "$_LOGDIR"
-sudo systemd-nspawn -D $WORKSPACE/$_ARCH/ -u manjaro sudo pacman -Syyu --noconfirm #&>> "$_LOGDIR"
+msg "===== Updating the rootfs ====="
+sudo systemd-nspawn -D $_ROOTFS/ -u manjaro sudo pacman -Syyu --noconfirm
 
 #cp package to rootfs
-msg "Copying build directory {$1/$2} to rootfs"
-#printf "\n\n===== Copying the build directory {$1/$2} to rootfs =====\n" >> "$_LOGDIR"
-sudo cp -rp /var/lib/jenkins/workspace/$2/$2/ $WORKSPACE/$_ARCH/home/manjaro/build/ #>> "$_LOGDIR"
+msg "===== Copying build directory {$1/$2} to rootfs ====="
+sudo cp -rp /var/lib/jenkins/workspace/$2/$2/ $_ROOTFS/home/manjaro/build/
 
 #build package
-msg "Building {$2}"
-#printf "\n\n===== Building {$2} =====\n" >> "$_LOGDIR"
-#sudo systemd-nspawn -D $_ARCH/ -u manjaro --chdir=/home/manjaro/ sudo chown -R manjaro build/
-sudo systemd-nspawn -D $WORKSPACE/$_ARCH/ -u manjaro --chdir=/home/manjaro/ sudo chmod -R 777 build/
-sudo systemd-nspawn -D $WORKSPACE/$_ARCH/ -u manjaro --chdir=/home/manjaro/build/ makepkg -scr --noconfirm #&>> "$_LOGDIR"
+msg "===== Building {$2} ====="
+sudo systemd-nspawn -D $_ROOTFS/ -u manjaro --chdir=/home/manjaro/ sudo chmod -R 777 build/
+sudo systemd-nspawn -D $_ROOTFS/ -u manjaro --chdir=/home/manjaro/build/ makepkg -scr --noconfirm
 #read -p "Press [Enter] to continue"
-if ls $WORKSPACE/$_ARCH/home/manjaro/build/*.pkg.tar.xz* 1> /dev/null 2>&1; then
-    #pull package our of rootfs
-    msg "Extracting finished package out of rootfs"
-    #printf "\n\n===== Extracting finished package out of rootfs =====\n" >> "$_LOGDIR"
-    cp $WORKSPACE/$_ARCH/home/manjaro/build/*.pkg.tar.xz $WORKSPACE/packages/$3/$1/ #&>> "$_LOGDIR"
+
+if ls $_ROOTFS/home/manjaro/build/*.pkg.tar.xz* 1> /dev/null 2>&1; then
+    #pull package out of rootfs
+    msg "!!!!! +++++ ===== Package Succeeded ===== +++++ !!!!!"
+    msg "===== Extracting finish/home/jenkins/build/ed package out of rootfs ====="
+    cp $_ROOTFS/home/manjaro/build/*.pkg.tar.xz $_PKGDIR/$3/$1/
+
+    #clean up rootfs
+    msg "===== Cleaning rootfs ====="
+    sudo rm -r $_ROOTFS/home/manjaro/build/ > /dev/null
+
 else
-    msg "Package failed to build"
-    #cat "$_LOGDIR" | grep -E 'error|ERROR|WARNING' 
+    msg "!!!!! ++++++ ===== Package failed to build ===== +++++ !!!!!"
+    msg "cleaning rootfs"
+    sudo rm -r $_ROOTFS/home/manjaro/build/ > /dev/null
+    exit 1
 fi
-
-#clean up rootfs
-msg "Cleaning rootfs"
-#printf "\n\n===== Cleaning rootfs =====\n" >> "$_LOGDIR"
-#msg "Entire build log can be found at $_LOGDIR"
-sudo rm -r $WORKSPACE/$_ARCH/home/manjaro/build/ > /dev/null
-
-#TODO
-
-#	Find a way to do sanity checks for existing files and error out if they are not accurate. For example, search for the arm7 (rootfs) and mark it good, search in the package folder for a PKGBUILD and mark it good, Search for log folders and mark it good, search for package directory for completed packages, and mark it good.
-
-#
